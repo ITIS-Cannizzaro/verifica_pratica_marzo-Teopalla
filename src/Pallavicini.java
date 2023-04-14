157c0f8386cfa9e023162223af75e93a04b8c288
prof-accordino <114094835+prof-accordino@users.noreply.github.com>
2023-04-14 09:26:49 +0200

    Correzioni

--- src/Pallavicini.java
+++ src/Pallavicini.java
@@ -51,7 +51,7 @@ public class Pallavicini
 		h[b]= temp;
 	}
 
-
+	// Accordino:  giusto! 2pt
 	static void MinimoDiArray()
 	{
 		int[]a= new int [10];
@@ -63,7 +63,7 @@ public class Pallavicini
 
 		for(int i=0; i<a.length; i++)
 		{
-			System.out.println(i + " il valore dell'array Ë: " + a[i]);
+			System.out.println(i + " il valore dell'array ÔøΩ: " + a[i]);
 		}
 
 		for(int i=0; i<a.length; i++)
@@ -72,21 +72,21 @@ public class Pallavicini
 			if (a[i]<numeroMinimo)
 			{
 				numeroMinimo=a[i];
-				System.out.println("la posizione Ë: " + i);
+				System.out.println("la posizione ÔøΩ: " + i);
 			}
 
 		}
-		System.out.println(" il valore minimo Ë: " + numeroMinimo );
+		System.out.println(" il valore minimo ÔøΩ: " + numeroMinimo );
 
 	}
-
+	// Accordino:  ci sei andato vicino ma hai sbagliato diverse cose: 1pt
 	static void  StampaPari() 
 	{
 		int[]a= new int [20];
 		int valoriCrescenti= 1;
 		for(int i=0; i<a.length; i++)
 		{
-			a[i]= (int)((Math.random())*101);
+			a[i]= (int)((Math.random())*101); // Accordino:  tra 1 e 100, dovevi fare +1
 		}
 		for(int i=0; i<a.length; i++)
 		{
@@ -98,16 +98,15 @@ public class Pallavicini
 			{
 				if (a[i]%2 == 0)
 				{
-					if (a[i]>a[j])
-						swap(a, valoriCrescenti, j);
+					if (a[i]>a[j])// Accordino:  cos√¨ li ordini in modo descrescente
+						swap(a, valoriCrescenti, j); // Accordino: perch√© valoricrescent??? devi scambiare i e j  
 				}
 
 			}
-			System.out.println(a[i]);
+			System.out.println(a[i]);// Accordino:  non puoi stampare mentre ordini, devi farlo dopo!
 		}
-
 	}
-
+	// Accordino:  va bene ma ti dovevi fermare a met√† dell'array: 3pt
 	static void  StampaZigZag() 
 	{
 		int[]a= new int [10];
@@ -119,7 +118,7 @@ public class Pallavicini
 
 		for(int i=0; i<a.length; i++)
 		{
-			System.out.println(i + " il valore dell'array Ë: " + a[i]);
+			System.out.println(i + " il valore dell'array ÔøΩ: " + a[i]);
 		}
 		for(int i=0; i<a.length; i++)
 		{
@@ -127,14 +126,16 @@ public class Pallavicini
 			System.out.println(a[i] + " , " + valoriDalbasso);
 		}
 	}
+	
+	// Accordino:  manca un ciclo, cos√¨ devi beccare non solo la parola ma anche la posizione: 0.5pt
 	static void ParoleMisteriose()
 	{
 
-		String []a= {"mi", "auguro", "di", "prendere", "sei"};
+		String []a= {"mi", "auguro", "di", "prendere", "sei"};// Accordino:  hai preso pi√π di sei, congratuluazioni!
 		System.out.println("ti sfido! trova almeno una delle 5 parole che ho pensato, hai 3 tentativi");
-		for(int i=0; i<a.length; i++)
+		for(int i=0; i<a.length; i++)// Accordino:  cos√¨ i tentativi sono 5 (quanto la lunghezza l'array)
 		{
-			System.out.println("inserisci la tua risposta");
+			System.out.println("inserisci la tua risposta"); // Accordino:  manca il ciclo che controlla tutte le parole dell'array, non solo una!s
 			if (a[i].equals(in.nextLine()))
 			{
 				System.out.println("bravo piccolo campione, hai vinto");
